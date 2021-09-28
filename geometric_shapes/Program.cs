d552807f94a162157c04d58a985498e34a93bc71
Jason Ryan <jamariryan89@yahoo.com>
2021-09-28 13:47:01 -0500

    Add project files.

---
+++ geometric_shapes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace geometric_shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = new Square()
+            {
+                Side1 = 5
+            };
+            Console.WriteLine($"Perimeter is {s1.Perimeter()}");
+            Console.WriteLine($"Perimeter is {s1.Perimeter()}");
+
+            var r1 = new Rect()
+            {
+                Side1 = 3,
+                Side2 = 5
+            };
+            Console.WriteLine($"Perimeter is {r1.Perimeter()}");
+            Console.WriteLine($"Area is {r1.Perimeter()}");
+
+            var q1 = new Quad()
+            {
+                Side1 = 3, Side2 = 4, Side3= 5, Side4 = 6
+            };
+            Console.WriteLine($"Perimeter is {q1.Perimeter()}");
+
+        }
+    }
+}
---
+++ geometric_shapes/Rect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometric_shapes
+{
+    class Rect
+    {
+        public int Side1 { get; set; }
+        public int Side2 { get; set; }
+
+        public int Area()
+        {
+            return Side1 * Side2;
+        }
+
+        
+        public int Perimeter()
+        {
+            return 2 * (Side1 + Side2);
+        }
+           
+    }
+}
